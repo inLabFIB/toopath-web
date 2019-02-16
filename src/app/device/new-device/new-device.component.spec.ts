254f889be3301bd1634516bb0e3f6172dc3ef908
Sergio <sergio.paredes@fib.upc.edu>
2019-02-16 19:28:32 +0100

    Fix NewDeviceComponent tests

--- src/app/device/new-device/new-device.component.spec.ts
+++ src/app/device/new-device/new-device.component.spec.ts
@@ -1,23 +1,26 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+import {FormsModule} from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing';
+import {Observable} from 'rxjs/internal/Observable';
 
 import { NewDeviceComponent } from './new-device.component';
+import {DeviceApiService} from '../services/device-api.service';
 
 describe('NewDeviceComponent', () => {
   let component: NewDeviceComponent;
-  let fixture: ComponentFixture<NewDeviceComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewDeviceComponent ]
-    })
-    .compileComponents();
+      declarations: [ NewDeviceComponent ],
+      imports: [ FormsModule, RouterTestingModule ],
+      providers: [{ provide: DeviceApiService, useValue: jasmine.createSpyObj(['postDevice']) } ],
+  });
+    const fixture = TestBed.createComponent(NewDeviceComponent);
+    component = fixture.componentInstance;
+    const deviceApiService = TestBed.get(DeviceApiService);
+    deviceApiService.postDevice.and.returnValue(new Observable());
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NewDeviceComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();

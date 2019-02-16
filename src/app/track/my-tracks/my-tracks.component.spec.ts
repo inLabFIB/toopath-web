2ea4ceb3c00ad3dad90e3b7ad31b228e4b100e5d
Sergio <sergio.paredes@fib.upc.edu>
2019-02-16 19:06:11 +0100

    Fix MyTrackComponent tests

--- src/app/track/my-tracks/my-tracks.component.spec.ts
+++ src/app/track/my-tracks/my-tracks.component.spec.ts
@@ -1,23 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 
 import { MyTracksComponent } from './my-tracks.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {TrackApiService} from '../services/track-api.service';
 
 describe('MyTracksComponent', () => {
   let component: MyTracksComponent;
-  let fixture: ComponentFixture<MyTracksComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MyTracksComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MyTracksComponent);
+      declarations: [MyTracksComponent],
+      imports: [RouterTestingModule],
+      providers: [{provide: TrackApiService, useValue: jasmine.createSpy('TrackApiService')}]
+    });
+    const fixture = TestBed.createComponent(MyTracksComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();

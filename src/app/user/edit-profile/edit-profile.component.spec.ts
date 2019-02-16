29e9b58784fe6612ac4676c8ddfd588c2cac8053
Sergio <sergio.paredes@fib.upc.edu>
2019-02-16 19:22:32 +0100

    Fix EditProfileComponent tests

--- src/app/user/edit-profile/edit-profile.component.spec.ts
+++ src/app/user/edit-profile/edit-profile.component.spec.ts
@@ -1,23 +1,31 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 
 import { EditProfileComponent } from './edit-profile.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AuthUserService} from '../../auth/services/auth-user.service';
+import {UserApiService} from '../services/user-api.service';
+import {Observable} from 'rxjs/internal/Observable';
+import {FormsModule} from '@angular/forms';
 
 describe('EditProfileComponent', () => {
   let component: EditProfileComponent;
-  let fixture: ComponentFixture<EditProfileComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EditProfileComponent ]
-    })
-    .compileComponents();
+      declarations: [ EditProfileComponent ],
+      imports: [ RouterTestingModule, FormsModule ],
+      providers: [
+        { provide: AuthUserService, useValue: jasmine.createSpyObj(['getCurrentUser']) },
+        { provide: UserApiService, useValue: jasmine.createSpyObj(['putUser']) },
+      ]
+
+    });
+    const fixture = TestBed.createComponent(EditProfileComponent);
+    component = fixture.componentInstance;
+    const userApiService = TestBed.get(UserApiService);
+    userApiService.putUser.and.returnValue(new Observable());
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EditProfileComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();

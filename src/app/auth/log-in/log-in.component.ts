7072e2b4f1c50330180a8ae2d2a5163b25864a54
Sergio <sergio.paredes@fib.upc.edu>
2019-02-16 19:45:33 +0100

    Fix LogInComponent tests and remove Google sign in form as it does not work

--- src/app/auth/log-in/log-in.component.ts
+++ src/app/auth/log-in/log-in.component.ts
@@ -1,6 +1,4 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {Router} from '@angular/router';
-import {GoogleSignInSuccess} from 'angular-google-signin';
 
 import {User} from '../../objects/user';
 import {AuthUserService} from '../services/auth-user.service';
@@ -14,25 +12,15 @@ export class AppLogInComponent implements OnInit {
 
   @ViewChild('logInButton', {read: ElementRef}) logInButton: ElementRef;
   user: User;
-  googleWidth: number;
-  private myClientId = '349015628188-7bpejun8mdc78d5chh7gk92u8dmttjl7.apps.googleusercontent.com';
 
-  constructor(private _authUserService: AuthUserService, private _router: Router) {
+  constructor(private _authUserService: AuthUserService) {
   }
 
   ngOnInit() {
     this.user = new User();
-    this.googleWidth = this.logInButton.nativeElement.offsetWidth;
   }
 
   async sendLogIn() {
     await this._authUserService.postLogin(this.user);
-  }
-
-  onGoogleSignInSuccess(event: GoogleSignInSuccess) {
-    const googleUser: gapi.auth2.GoogleUser = event.googleUser;
-    const token: string = googleUser.getAuthResponse().id_token;
-    const profile: gapi.auth2.BasicProfile = googleUser.getBasicProfile();
-    this._authUserService.postGoogleLogIn(token, profile.getEmail(), profile.getName());
   }
 }
